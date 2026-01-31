02481be49583ea672984751521ff459d5c95dde7
Andrei Cioara <andrei@cioara.me>
2026-01-31 08:24:19 -0800

    More refactor

--- src/docker_command.sh
+++ src/docker_command.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
 cd /workspace/src/pandoc/
-set -xe
+set -e
 
 today=$(date +%Y-%m-%d)
+echo -e "\n\n"
 
-
-# English Print
+echo "Generating the English Print Version"
 /usr/local/bin/pandoc \
     --defaults=settings.yaml \
     --defaults=settings-language-en.yaml \
+    --defaults=settings-format-print.yaml \
     --defaults=input_files_english.yaml \
     --include-in-header=header.tex \
     --include-in-header=header-language-en.tex \
     -o "/workspace/output/StoriesFrom2025 - English - Print - $today.pdf"
+echo -e "\n\n"
 
-# Romanian Print
+echo "Generating the Romanian Print Version"
 /usr/local/bin/pandoc \
     --defaults=settings.yaml \
     --defaults=settings-language-ro.yaml \
+    --defaults=settings-format-print.yaml \
     --defaults=input_files_romanian.yaml \
     --include-in-header=header.tex \
     --include-in-header=header-language-ro.tex \
     -o "/workspace/output/StoriesFrom2025 - Romanian - Print - $today.pdf"
+echo -e "\n\n"
 
 exit 0
